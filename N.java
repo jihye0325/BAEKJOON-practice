403a2177c85ff6e73f8f6c50005f181cccc78f4f
jihye0325 <hadocho1025@gmail.com>
2021-08-20 15:37:35 +0900

    2741

---
+++ N.java
@@ -0,0 +1,19 @@
+package for_practice;
+
+import java.util.Scanner;
+
+public class N2741 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		if(N <= 100000)	{
+			for(int i = 1; i <= N; i++) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
