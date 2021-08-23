014a293c3f368fd08e422c8d73acfca5f3c98fd2
jihye0325 <hadocho1025@gmail.com>
2021-08-24 01:41:12 +0900

    10171

---
+++ cat_dog.java
@@ -0,0 +1,20 @@
+package practice;
+
+public class cat_dog10171 {
+
+	public static void main(String[] args) {
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+		System.out.println();
+		System.out.println();
+		
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+}
