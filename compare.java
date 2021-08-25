52a4ef311dadd861e35625e15aa0c68dc2424245
jihye0325 <hadocho1025@gmail.com>
2021-08-25 21:42:53 +0900

    2908

---
+++ compare.java
@@ -0,0 +1,20 @@
+package practice;
+
+import java.util.Scanner;
+
+public class compare2908 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		StringBuilder sb = new StringBuilder("");
+		
+		A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
+		B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
+		
+		System.out.println(A > B ? A : B);
+	}
+
+}
