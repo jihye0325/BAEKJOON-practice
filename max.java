76c1357b2a7ae5ba3c46d62910f170c294c6c50f
jihye0325 <hadocho1025@gmail.com>
2021-08-30 14:44:32 +0900

    2562

---
+++ max.java
@@ -0,0 +1,27 @@
+package practice;
+
+import java.util.Scanner;
+
+public class max2562 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] arr = new int[9];
+		
+		for(int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+		
+		int max = 0;
+		int index = 0;
+		for(int i = 0; i < arr.length; i++) {
+			if(max < arr[i]) {
+				max = arr[i];
+				index = i;
+			}
+		}
+		System.out.println(max);	
+		System.out.println(index + 1);
+	}
+
+}
