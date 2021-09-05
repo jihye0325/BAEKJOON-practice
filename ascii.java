e288ea800ef50f4b164e4c20b02316e96c2ca2bb
jihye0325 <hadocho1025@gmail.com>
2021-09-05 18:22:41 +0900

    11654

---
+++ ascii.java
@@ -0,0 +1,16 @@
+package practice;
+
+import java.util.Scanner;
+
+public class ascii11654 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int ch = sc.nextLine().charAt(0);
+		
+		System.out.println(ch);
+
+	}
+
+}
