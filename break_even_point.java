0e39981ff0b26e77a575303e6ab0c2e9ea93f8f1
jihye0325 <hadocho1025@gmail.com>
2021-10-04 10:49:50 +0900

    1712

---
+++ break_even_point.java
@@ -0,0 +1,22 @@
+package practice;
+
+import java.util.Scanner;
+
+public class break_even_point1712 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		
+		if(C <= B) {
+			System.out.println("-1");
+		} else {
+			System.out.println(A/(C-B) + 1 );
+		}
+
+	}
+
+}
