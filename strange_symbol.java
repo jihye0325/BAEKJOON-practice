2b1e05c66959590d9c1f4fbabba1edfd03650e9c
jihye0325 <hadocho1025@gmail.com>
2021-10-12 18:46:54 +0900

    15964

---
+++ strange_symbol.java
@@ -0,0 +1,17 @@
+package practice;
+
+import java.util.Scanner;
+
+public class strange_symbol15964 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		System.out.println((A+B) * (A-B));
+
+	}
+
+}
