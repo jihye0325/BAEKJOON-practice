e22c0029eeb18e266dc585501d15901c978bfa6b
jihye0325 <hadocho1025@gmail.com>
2021-11-07 18:16:09 +0900

    11719

---
+++ print2.java
@@ -0,0 +1,17 @@
+package practice;
+
+import java.util.Scanner;
+
+public class print11719 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextLine()) {
+			String str = sc.nextLine();
+			System.out.println(str);
+		}
+
+	}
+
+}
